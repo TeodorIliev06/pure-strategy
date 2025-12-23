9fe82414404b779b95c3d1f653677bd69fd08937
TeodorIliev06 <tedo2505@gmail.com>
2025-12-23 23:55:59 +0200

    chore: add initially defined constants

---
+++ PureStrategy/PureStrategy.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+const int USERNAME_MAX_LEN = 32;
+const int PASSWORD_MAX_LEN = 32;
+
+const int MAX_CARDS_IN_SUIT = 13;
+
+const int CARD_MIN_VALUE = 1;
+const int CARD_MAX_VALUE = 13;
+
+const int INITIAL_CARDS_CAPACITY = 4;
+
+int main()
+{
+
+}
