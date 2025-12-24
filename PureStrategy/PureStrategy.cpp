1b0ac10718eac2eb9538463eb4385731de90e74c
TeodorIliev06 <tedo2505@gmail.com>
2025-12-24 12:03:09 +0200

    feat: implement card dealing logic
    - create player struct
    - deal cards to players
    - shuffle prize deck

--- PureStrategy/PureStrategy.cpp
+++ PureStrategy/PureStrategy.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <cstdlib>
 
 const int USERNAME_MAX_LEN = 32;
 const int PASSWORD_MAX_LEN = 32;
 
-const int MAX_CARDS_IN_SUIT = 13;
+const int CARDS_IN_SUIT = 13;
 
 const int CARD_MIN_VALUE = 1;
 const int CARD_MAX_VALUE = 13;
@@ -21,6 +22,15 @@ struct CardArray
 	int count;
 	int capacity;
 };
+
+struct Player
+{
+	char username[USERNAME_MAX_LEN];
+	CardArray hand;
+	CardArray rewards;
+};
+
+// ==== Cards functions ====
 
 void InitializeCardArray(CardArray& arr)
 {
@@ -80,7 +90,94 @@ void FreeCardArray(CardArray& array)
 	array.capacity = 0;
 }
 
+// ==== Dealing logic ====
+
+void InitializePlayer(Player& player, const char* name)
+{
+	int i = 0;
+
+	while (name[i] != '\0' && i < USERNAME_MAX_LEN - 1)
+	{
+		player.username[i] = name[i];
+		i++;
+	}
+
+	player.username[i] = '\0';
+
+	InitializeCardArray(player.hand);
+	InitializeCardArray(player.rewards);
+}
+
+void DealCardsToPlayer(Player& player)
+{
+	for (int value = 1; value <= CARDS_IN_SUIT; value++)
+	{
+		Card card;
+		card.value = value;
+		AddCard(player.hand, card);
+	}
+}
+
+void CreatePrizeDeck(CardArray& prizeDeck)
+{
+	InitializeCardArray(prizeDeck);
+
+	for (int value = 1; value <= CARDS_IN_SUIT; value++)
+	{
+		Card card;
+		card.value = value;
+		AddCard(prizeDeck, card);
+	}
+}
+
+void ShufflePrizeDeck(CardArray& deck)
+{
+	for (int i = deck.count - 1; i > 0; i--)
+	{
+		int j = rand() % (i + 1);
+
+		Card temp = deck.data[i];
+		deck.data[i] = deck.data[j];
+		deck.data[j] = temp;
+	}
+}
+
 int main()
 {
+	srand((unsigned int)time(NULL));
 
+	Player playerOne;
+	Player playerTwo;
+	CardArray prizeDeck;
+
+	InitializePlayer(playerOne, "Player1");
+	InitializePlayer(playerTwo, "Player2");
+
+	DealCardsToPlayer(playerOne);
+	DealCardsToPlayer(playerTwo);
+
+	CreatePrizeDeck(prizeDeck);
+	ShufflePrizeDeck(prizeDeck);
+
+	std::cout << "Player 1 hand:" << std::endl;
+	for (int i = 0; i < playerOne.hand.count; i++)
+	{
+		std::cout << playerOne.hand.data[i].value << " ";
+	}
+	std::cout << std::endl;
+
+	std::cout << "Prize deck:" << std::endl;
+	for (int i = 0; i < prizeDeck.count; i++)
+	{
+		std::cout << prizeDeck.data[i].value << " ";
+	}
+	std::cout << std::endl;
+
+	FreeCardArray(playerOne.hand);
+	FreeCardArray(playerOne.rewards);
+	FreeCardArray(playerTwo.hand);
+	FreeCardArray(playerTwo.rewards);
+	FreeCardArray(prizeDeck);
+
+	return 0;
 }
