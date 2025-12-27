c50bbb9acf31961596e392be3db36ecc4fe1fee6
Teodor Iliev <tedo2505@gmail.com>
2025-12-27 21:23:04 +0200

    feat: implement i/o operations

---
+++ PureStrategy/UserProfile.cpp
@@ -0,0 +1,175 @@
+/**
+*
+* Solution to course project #1
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2025/2026
+*
+* @author Teodor Iliev
+* @idnumber 4MI0600570
+* @compiler VS
+*
+* <Implementation of User Profile logic>
+*
+*/
+
+#include <iostream>
+#include <fstream>
+
+#include "UserProfile.h"
+
+std::string GetFilename(const std::string& username)
+{
+	return username + ".txt";
+}
+
+bool UserExists(const std::string& username)
+{
+	std::ifstream file(GetFilename(username));
+	return file.good();
+}
+
+void SaveUserProfile(const UserProfile& profile)
+{
+	std::ofstream file(GetFilename(profile.username));
+	if (!file.is_open()) return;
+
+	file << profile.username << "\n";
+	file << profile.password << "\n";
+	file << profile.totalGamesPlayed << "\n";
+	file << profile.totalGamesWon << "\n";
+
+	// Save vector size first
+	file << profile.opponentStats.size() << "\n";
+
+	for (size_t i = 0; i < profile.opponentStats.size(); i++)
+	{
+		file << profile.opponentStats[i].opponentName << "\n";
+		file << profile.opponentStats[i].gamesPlayed << "\n";
+		file << profile.opponentStats[i].gamesWon << "\n";
+	}
+	file.close();
+}
+
+bool LoadUserProfile(const std::string& username, UserProfile& profile)
+{
+	std::ifstream file(GetFilename(username));
+
+	if (!file.is_open())
+		return false;
+
+	std::getline(file, profile.username);
+	std::getline(file, profile.password);
+
+	file >> profile.totalGamesPlayed;
+	file >> profile.totalGamesWon;
+
+	size_t count;
+	file >> count;
+
+	file.ignore(10000, '\n');
+	profile.opponentStats.clear();
+
+	for (size_t i = 0; i < count; i++)
+	{
+		OpponentStat stat;
+
+		std::getline(file, stat.opponentName);
+
+		file >> stat.gamesPlayed;
+		file >> stat.gamesWon;
+
+		file.ignore(10000, '\n');
+		profile.opponentStats.push_back(stat);
+	}
+	return true;
+}
+
+void UpdateOpponentStat(UserProfile& profile, const std::string& opponentName, bool won)
+{
+	bool found = false;
+	for (size_t i = 0; i < profile.opponentStats.size(); i++)
+	{
+		if (profile.opponentStats[i].opponentName == opponentName)
+		{
+			profile.opponentStats[i].gamesPlayed++;
+
+			if (won)
+				profile.opponentStats[i].gamesWon++;
+
+			found = true;
+			break;
+		}
+	}
+
+	if (!found)
+	{
+		OpponentStat newStat;
+		newStat.opponentName = opponentName;
+		newStat.gamesPlayed = 1;
+		newStat.gamesWon = (won ? 1 : 0);
+		profile.opponentStats.push_back(newStat);
+	}
+}
+
+void RegisterUser(UserProfile& activeProfile)
+{
+	std::string username, password;
+
+	std::cin.ignore(10000, '\n');
+
+	std::cout << "Enter new username: ";
+	std::getline(std::cin, username);
+
+	if (UserExists(username))
+	{
+		std::cout << "Error: User already exists.\n";
+		return;
+	}
+
+	std::cout << "Enter password: ";
+	std::getline(std::cin, password);
+
+	// Reset profile data
+	activeProfile.username = username;
+	activeProfile.password = password;
+	activeProfile.totalGamesPlayed = 0;
+	activeProfile.totalGamesWon = 0;
+	activeProfile.opponentStats.clear();
+
+	SaveUserProfile(activeProfile);
+	std::cout << "Registration successful!\n";
+}
+
+bool LoginUser(UserProfile& activeProfile)
+{
+	std::string username, password;
+
+	std::cin.ignore(10000, '\n');
+
+	std::cout << "Username: ";
+	std::getline(std::cin, username);
+
+	if (!UserExists(username))
+	{
+		std::cout << "Error: User not found.\n";
+		return false;
+	}
+
+	UserProfile temp;
+	if (!LoadUserProfile(username, temp))
+		return false;
+
+	std::cout << "Password: ";
+	std::getline(std::cin, password);
+
+	if (temp.password == password)
+	{
+		activeProfile = temp;
+		std::cout << "Login successful!\n";
+		return true;
+	}
+
+	std::cout << "Error: Incorrect password.\n";
+	return false;
+}
---
+++ PureStrategy/UserProfile.h
@@ -0,0 +1,47 @@
+/**
+*
+* Solution to course project #1
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2025/2026
+*
+* @author Teodor Iliev
+* @idnumber 4MI0600570
+* @compiler VS
+*
+* <Header file for user profile data and file I/O>
+*
+*/
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+struct OpponentStat
+{
+	std::string opponentName;
+	int gamesPlayed;
+	int gamesWon;
+};
+
+struct UserProfile
+{
+	std::string username;
+	std::string password;
+	int totalGamesPlayed;
+	int totalGamesWon;
+	std::vector<OpponentStat> opponentStats;
+};
+
+bool UserExists(const std::string& username);
+
+void SaveUserProfile(const UserProfile& profile);
+
+bool LoadUserProfile(const std::string& username, UserProfile& profile);
+
+void UpdateOpponentStat(UserProfile& profile, const std::string& opponentName, bool won);
+
+void RegisterUser(UserProfile& activeProfile);
+
+bool LoginUser(UserProfile& activeProfile);
